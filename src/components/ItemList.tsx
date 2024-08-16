c407e1045319670bbd65be5b19b8f9d9a2d92b00
Kristjan Lääne <xonproject@gmail.com>
2024-08-16 22:41:29 +0300

    itemlist start

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,3 +1,41 @@
+const initialItems = [
+  {
+    name: 'good mood',
+    packed: true,
+  },
+  {
+    name: 'passport',
+    packed: false,
+  },
+  {
+    name: 'phone charger',
+    packed: false,
+  },
+];
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {initialItems.map((item) => {
+        return <Item key={item.name} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+type ItemProps = {
+  item: {
+    name: string;
+    packed: boolean;
+  };
+};
+
+function Item({ item }: ItemProps) {
+  return (
+    <li className='item'>
+      <label>
+        <input type='checkbox' checked={item.packed} /> {item.name}
+      </label>
+    </li>
+  );
 }
