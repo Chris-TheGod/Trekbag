5091a07ccd948555610ef9a58031264e07f4bb77
Kristjan Lääne <xonproject@gmail.com>
2024-08-16 20:59:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import BackgroundHeading from './components/BackgroundHeading';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <BackgroundHeading />
+
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
