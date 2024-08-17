bd804c55471237c078779e5f9370edc3456af495
Kristjan Lääne <xonproject@gmail.com>
2024-08-17 11:57:01 +0300

    add item

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Button from './Button';
 
 import { ItemType } from '../lib/types';
@@ -9,32 +9,37 @@ type AddItemFormProps = {
 
 export default function AddItemForm({ setItems }: AddItemFormProps) {
   const [itemText, setItemText] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null!);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert(`Item can't be empty`);
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem: ItemType = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((prev) => [...prev, newItem]);
+    setItemText('');
+  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-
-        if (!itemText) {
-          alert(`Item can't be empty`);
-          return;
-        }
-
-        const newItem: ItemType = {
-          id: new Date().getTime(),
-          name: itemText,
-          packed: false,
-        };
-
-        setItems((prev) => [...prev, newItem]);
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
       <input
+        ref={inputRef}
         value={itemText}
         onChange={(e) => {
           setItemText(e.target.value);
         }}
+        autoFocus
       />
       <Button>Add to list</Button>
     </form>
